7db42c8a1e8ce3be75310d5c341af4f98cbfaf3c
Stephan Bönnemann <stephan@boennemann.me>
2015-06-18 17:12:28 -0700

    test(commit-analyzer): initial suite

---
+++ test/specs/index.js
@@ -0,0 +1,67 @@
+const { test } = require('tap')
+
+const analyzer = require('../../dist')
+
+test('derive version number from commits', (t) => {
+  t.test('no change', (tt) => {
+    tt.plan(2)
+
+    analyzer({}, [{
+      hash: 'asdf',
+      message: 'chore: build script'
+    }], (err, type) => {
+      tt.error(err)
+      tt.is(type, null)
+    })
+  })
+
+  t.test('patch version', (tt) => {
+    tt.plan(2)
+
+    analyzer({}, [{
+      hash: 'asdf',
+      message: 'fix: nasty bug'
+    }, {
+      hash: '1234',
+      message: 'fix(scope): even nastier bug'
+    }], (err, type) => {
+      tt.error(err)
+      tt.is(type, 'patch')
+    })
+  })
+
+  t.test('minor/feature version', (tt) => {
+    tt.plan(2)
+
+    analyzer({}, [{
+      hash: 'asdf',
+      message: 'fix: nasty bug'
+    }, {
+      hash: '1234',
+      message: 'feat(scope): cool feature'
+    }], (err, type) => {
+      tt.error(err)
+      tt.is(type, 'minor')
+    })
+  })
+
+  t.test('major/breaking version', (tt) => {
+    tt.plan(2)
+
+    analyzer({}, [{
+      hash: 'qwer',
+      message: 'feat(something): even cooler feature\nBREAKING CHANGE: everything so broken'
+    }, {
+      hash: '1234',
+      message: 'feat(scope): cool feature'
+    }, {
+      hash: 'asdf',
+      message: 'fix: nasty bug'
+    }], (err, type) => {
+      tt.error(err)
+      tt.is(type, 'major')
+    })
+  })
+
+  t.end()
+})
